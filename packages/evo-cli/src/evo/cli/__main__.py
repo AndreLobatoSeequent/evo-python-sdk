082b5356e90ee4420f19f015a4041baceb0dd2e9
Andre Lobato <Andre.Lobato@bentley.com>
2026-09-15 12:58:57 +1200

    Add files API and refresh token logic

--- packages/evo-cli/src/evo/cli/__main__.py
+++ packages/evo-cli/src/evo/cli/__main__.py
@@ -16,6 +16,7 @@ from typing import Optional
 import typer
 
 from evo.cli.auth import app as auth_app
+from evo.cli.files import app as files_app
 from evo.cli.objects import app as objects_app
 from evo.cli.output import OutputFormat, init as init_output
 
@@ -27,6 +28,7 @@ app = typer.Typer(
 
 app.add_typer(auth_app, name="auth")
 app.add_typer(objects_app, name="objects")
+app.add_typer(files_app, name="files")
 
 
 @app.callback()
